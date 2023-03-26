efbe29359a7ae3f6ac11b0608e220e2401c9bcda
AdoNoColor <macccich@gmail.com>
2023-03-26 21:00:14 +0300

    feat: handlebars templating

--- public/scripts/timeLoad.js
+++ public/scripts/timeLoad.js
@@ -3,7 +3,6 @@
     window.addEventListener("load", () => {
         let timeEnd = Date.now();
         let milliseconds = timeEnd - timeStart
-        let seconds = milliseconds / 1000;
-        document.getElementById("loading").innerText = `${seconds} s`;
+        document.getElementById("loading").innerText = `${milliseconds} ms (client)`;
     });
 })();
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import * as hbs from 'express-handlebars';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.engine('hbs', hbs({
+    extname: 'hbs',
+    partialsDir: join(__dirname, '..', 'views/partials'),
+    layoutsDir: join(__dirname, '..', 'views/layouts'),
+    defaultLayout: 'main',
+  }));
+
   app.setViewEngine('hbs');
   await app.listen(3005);
 }
