627a3da142b87ff1cc0102bddf8f62bcb7ad8a29
AdoNoColor <macccich@gmail.com>
2023-03-28 13:32:32 +0300

    feat: server timeload

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import * as hbs from 'express-handlebars';
+import { ResponseTimeInterceptor } from './timeloadServer';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+  app.useGlobalInterceptors(new ResponseTimeInterceptor());
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.engine('hbs', hbs({
